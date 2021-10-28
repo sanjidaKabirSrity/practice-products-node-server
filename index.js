bd33a8989a6da9299b5ab8153c0246723cc37308
sanjidaKabirSrity <sanjida.kabir.srity@gmail.com>
2021-10-28 12:38:50 +0600

    provlem sove

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const cors = require("cors");
 
 const app = express();
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 
 // middleWare 
 app.use(cors());
@@ -12,6 +12,25 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bonkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+   try {
+     await client.connect();
+     const database = client.db("products_practice");
+     const produtsCollection = database.collection("products_data");
+
+     // GET API
+     app.get('/products' , async (req, res)=> {
+        const result = await produtsCollection.find({}).toArray();
+        console.log('hitting the server' , result)
+        res.send(result);
+     })
+
+   } finally {
+     // await client.close();
+   }
+ }
+run().catch(console.dir);
 
 app.get("/" , (req, res) => {
    res.send("Hello world");
